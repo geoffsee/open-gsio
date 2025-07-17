ce9bc4db07f238d8c5b15711e1d8410ab6a26368
geoffsee <>
2025-07-17 14:11:15 -0400

    "Swap default states for `mapActive` and `aiActive` in LandingComponent"

--- packages/client/src/components/landing-component/LandingComponent.tsx
+++ packages/client/src/components/landing-component/LandingComponent.tsx
@@ -8,8 +8,8 @@ import Tweakbox from './Tweakbox.tsx';
 
 export const LandingComponent: React.FC = () => {
   const [intensity, setIntensity] = useState(0.99);
-  const [mapActive, setMapActive] = useState(false);
-  const [aiActive, setAiActive] = useState(true);
+  const [mapActive, setMapActive] = useState(true);
+  const [aiActive, setAiActive] = useState(false);
 
   const component = useComponent();
   const { setEnabledComponent } = component;
